3eaa0239ace2d3cc8a46bc45c5d99a1d07b2822f
herorshi <metasitstar@gmail.com>
2022-10-13 20:20:23 +0700

    set-vercel

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const corsConfig = {
   // origin: function(origin, callback) {
   //   return callback(null, true);
   // },
-  origin: "http://localhost:3000",
+  origin: "https://fe-exam-goventure.vercel.app",
   optionsSuccessStatus: 200,
   credentials: true
 };
@@ -20,19 +20,6 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 const { getRouter } = require("./router");
-app.get("/s", (req, res) => {
-  res.cookie("bsaSession", req.session.id, { httpOnly: false });
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.send("set");
-});
-
-app.get("/td", function(req, res) {
-  res.cookie("name", "express");
-  console.log("td");
-  console.log(req.cookies, "req.cookies");
-  res.end("end");
-});
 
 getRouter(app);
 var server = app.listen(4000, function() {
